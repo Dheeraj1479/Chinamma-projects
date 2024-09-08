76ee0a636b969c34026462e174c8bca317a1946a
Dheeraj1479 <dheeraj14krishna@gmail.com>
2024-09-08 18:34:03 +0100

    Day-9
    Write a program that calculates the sum of digits of a given number

---
+++ Day-9.py
@@ -0,0 +1,7 @@
+#Write a program that calculates the sum of digits of a given number
+
+print("Enter the two number that you want to add")
+num1 = int(input())
+num2 = int(input())
+
+print(f"The sum of the two numbers are: {num1+num2}")
